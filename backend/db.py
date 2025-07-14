0c5a1d266776847856bc05248fe3522822c0feeb
Elyor <thespacetab1@gmail.com>
2025-07-14 14:42:37 +0500

    chore: оставлена только папка frontend для деплоя

--- backend/db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('tashmall.db')
-cursor = conn.cursor()
-cursor.execute('SELECT * FROM users')  # или shops, если аккаунты в этой таблице
-for row in cursor.fetchall():
-    print(row)
-conn.close()
